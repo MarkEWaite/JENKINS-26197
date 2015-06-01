e4bed9ee2df48d19e624288b64d95d104a7ad18c
Mark Waite <mark.earl.waite@gmail.com>
2015-05-31 19:34:32 -0600

    Add branch_ops.py - simpler than ant

---
+++ branch_ops.py
@@ -0,0 +1,46 @@
+#! /usr/bin/python
+
+import subprocess
+
+#----------------------------------------------------------------------------
+
+def checkout_master_branch():
+    subprocess.check_call(["git", "checkout", "-f", "master"])
+
+#----------------------------------------------------------------------------
+
+def list_a_branch():
+    "List one existing remote branch name that starts with 'branch', return None otherwise"
+    branch_ref = subprocess.check_output(["git", "ls-remote", "--heads", "origin", "branch*"]).split()
+    prefix = "refs/heads/"
+    if len(branch_ref) > 1:
+        if branch_ref[1].startswith(prefix):
+            return branch_ref[1][len(prefix):]
+    return None
+
+#----------------------------------------------------------------------------
+
+def remove_a_branch():
+    branch_name = list_a_branch()
+    if not branch_name:
+        return
+    subprocess.check_call(["git", "branch", "-D", branch_name])
+    subprocess.check_call(["git", "push", "origin", ":" + branch_name])
+
+#----------------------------------------------------------------------------
+
+def create_and_push_a_branch():
+    pass
+
+#----------------------------------------------------------------------------
+
+def branch_ops():
+    checkout_master_branch()
+    remove_a_branch()
+    create_and_push_a_branch()
+
+#----------------------------------------------------------------------------
+
+if __name__ == "__main__":
+    branch_ops()
+    exit(0)
