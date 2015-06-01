e534f08eb0234751fbe504af126d93a17f0148c3
Mark Waite <mark.earl.waite@gmail.com>
2015-05-31 20:00:51 -0600

    Add function comments

--- branch_ops.py
+++ branch_ops.py
@@ -8,6 +8,7 @@ import time
 #----------------------------------------------------------------------------
 
 def checkout_master_branch():
+    "Checkout master branch (force) and clean working directory"
     subprocess.check_call(["git", "checkout", "-f", "master"])
     subprocess.check_call(["git", "clean", "-xffd"])
 
@@ -25,6 +26,7 @@ def list_a_branch():
 #----------------------------------------------------------------------------
 
 def remove_a_branch():
+    "Remove one remote branch"
     branch_name = list_a_branch()
     if not branch_name:
         return
@@ -34,6 +36,7 @@ def remove_a_branch():
 #----------------------------------------------------------------------------
 
 def create_and_push_a_branch():
+    "Create a branch and push it to origin"
     suffix = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for i in range(2))
     branch_name = "branch-" + time.strftime("%Y-%m-%d-%H-%M-%S") + "-" + suffix
     subprocess.check_call(["git", "checkout", "-b", branch_name, "origin/master"])
@@ -47,7 +50,7 @@ def create_and_push_a_branch():
 
 def branch_ops():
     checkout_master_branch()
-    # remove_a_branch()
+    remove_a_branch()
     create_and_push_a_branch()
     checkout_master_branch()
 
